91992ec236b0371b6097a6686cf95e7f89a1e061
Elementary - Clément <otipassplaystore@gmail.com>
2020-07-08 10:13:46 +0200

    Add DE 64*64

--- flags/flat/64/index.js
+++ flags/flat/64/index.js
@@ -4,4 +4,5 @@ export const GB = require('./GB.png');
 export const IL = require('./IL.png');
 export const IT = require('./IT.png');
 export const SA = require('./SA.png');
+export const DE = require('./DE.png');
 export const basqueCountry = require('./_basque-country.png');
